c56c41689f1c0d445c348f3465f3cbfa07189fa1
brand421 <57531511+brand421@users.noreply.github.com>
2023-05-31 10:22:31 -0400

    add env and update app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const MongoClient = require('mongodb').MongoClient;
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
 const url = 'mongodb://0.0.0.0:27017';
+
+const app = express();
+mongoose.connect(url, { useNewUrlParser: true });
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(express.static("public"));
 
 const dbName = wikiDB;
 
